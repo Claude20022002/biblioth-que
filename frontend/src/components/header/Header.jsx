d16f320cd1354f196074e9fd9466d117cdc3467b
Claude20022002 <clusamote@gmail.com>
2025-02-04 18:41:53 +0100

    Construction du home

--- frontend/src/components/header/Header.jsx
+++ frontend/src/components/header/Header.jsx
@@ -1,30 +1,65 @@
-import React from "react";
-import { Stack } from "@mui/material";
+import React, { useState } from "react";
+import { Box, Stack } from "@mui/material";
 import { Link } from "react-router-dom";
 import "./header.css";
 import Logo from "/logo.svg";
+import { IoMenu } from "react-icons/io5";
+import { IoMdClose } from "react-icons/io";
 
 export default function Header() {
+    const [open, setOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setOpen(!open);
+    };
+
+    const iconStyle = {
+        width: "100%",
+        height: "100%",
+    };
+
     return (
-        <div>
-            <header>
-                <Stack className="logo">
+        <header>
+            <Stack
+                className="logo"
+                sx={{
+                    width: { xs: "90px !important", sm: "130px !important" },
+                }}
+            >
+                <Link to="/">
                     <img src={Logo} alt="Logo" />
-                </Stack>
-                <Stack className="nav" component="nav">
-                    <ul>
-                        <li>
-                            <Link to="/">Accueil</Link>
-                        </li>
-                        <li>
-                            <Link to="/librairie">Librairie</Link>
-                        </li>
-                        <li>
-                            <Link to="/contact">Contact</Link>
-                        </li>
-                    </ul>
-                </Stack>
-            </header>
-        </div>
+                </Link>
+            </Stack>
+
+            <Stack className="nav">
+                {/* Icône Menu Burger */}
+                <Box className="menu" onClick={toggleMenu}>
+                    {open ? (
+                        <IoMdClose style={iconStyle} />
+                    ) : (
+                        <IoMenu style={iconStyle} />
+                    )}
+                </Box>
+
+                {/* Liste des liens avec effet de slide */}
+                <ul className={open ? "isActive" : ""}>
+                    <li>
+                        <Link to="/" onClick={toggleMenu}>
+                            Home
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/librairie" onClick={toggleMenu}>
+                            Bookstore
+                        </Link>
+                    </li>
+                    <li>
+                        <Link to="/contact" onClick={toggleMenu}>
+                            Contact
+                        </Link>
+                    </li>
+                </ul>
+            </Stack>
+        </header>
     );
 }
--- frontend/src/pages/Accueil.jsx
+++ frontend/src/pages/Accueil.jsx
@@ -1,5 +1,51 @@
+import { Stack, Typography } from "@mui/material";
 import React from "react";
+import "../style/accueil.css";
+import SplitText from "../components/splitText/SplitText";
 
 export default function Accueil() {
-    return <div>Accueil</div>;
+    const handleAnimationComplete = () => {
+        console.log("Toutes les lettres ont été animées !");
+    };
+
+    return (
+        <div className="body">
+            <Stack
+                component="section"
+                className="sec-1"
+                sx={{
+                    flexDirection: "row",
+                    flexWrap: "wrap",
+                    alignItems: "center",
+                    justifyContent: "space-around",
+                    height: "90vh",
+                }}
+            >
+                <SplitText
+                    text="Your New Library"
+                    className="text-2xl font-semibold text-center box-1"
+                    delay={150}
+                    animationFrom={{
+                        opacity: 0,
+                        transform: "translate3d(0,50px,0)",
+                    }}
+                    animationTo={{
+                        opacity: 1,
+                        transform: "translate3d(0,0,0)",
+                    }}
+                    easing="easeOutCubic"
+                    threshold={0.2}
+                    rootMargin="-50px"
+                    onLetterAnimationComplete={handleAnimationComplete}
+                />
+                <Typography
+                    className="box-2"
+                    variant="h6"
+                    sx={{ textAlign: "center", margin: "auto 1rem" }}
+                >
+                    Découvrez une nouvelle façon de lire et de nouveau livre
+                </Typography>
+            </Stack>
+        </div>
+    );
 }
